f548de25339d3e9f046b627906a8daaf704ecb66
Donggeon Moon <nullsafer@naver.com>
2023-11-13 17:06:09 +0900

    refactor: 컨트롤러 메서드 분리

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,7 +1,7 @@
 package christmas.controller;
 
-import christmas.dto.EventBadgeDto;
 import christmas.dto.BenefitsDto;
+import christmas.dto.EventBadgeDto;
 import christmas.dto.GiveawayMenuDto;
 import christmas.dto.OrderDto;
 import christmas.dto.PostDiscountAmountDto;
@@ -15,7 +15,6 @@ import christmas.view.ChristmasOutputView;
 public class ChristmasController extends AbstractRetry {
     private final ChristmasOutputView outputView;
     private final ChristmasInputView inputView;
-
     private final ChristmasService service;
 
     private ChristmasController(
@@ -37,31 +36,88 @@ public class ChristmasController extends AbstractRetry {
     }
 
     public void start() {
+        showEventPlannerMessage();
+
+        VisitDateDto visitDateDto = askExpectedVisitDate();
+        OrderDto orderDto = askMenuAndCount();
+
+        showEventBenefitPreview(orderDto, visitDateDto);
+    }
+
+    private void showEventPlannerMessage() {
         outputView.printEventPlannerMessage();
-        VisitDateDto visitDateDto = run(() -> {
+    }
+
+    private VisitDateDto askExpectedVisitDate() {
+        return run(() -> {
             int inputDay = inputView.readVisitDate();
             return service.generateVisitDate(inputDay);
         });
+    }
 
-        OrderDto orderDto = run(() -> {
+    private OrderDto askMenuAndCount() {
+        return run(() -> {
             String inputMenuAndCount = inputView.readMenuAndCount();
             return service.generateOrderedMenu(inputMenuAndCount);
         });
+    }
+
+    private void showEventBenefitPreview(
+            final OrderDto orderDto,
+            final VisitDateDto visitDateDto
+    ) {
         outputView.printEventBenefitPreview();
+
+        showOrderedMenu(orderDto);
+        PreDiscountAmountDto preDiscountAmountDto = showPreDiscountAmount(orderDto);
+
+        BenefitsDto benefitsDto = showGiveawayAndBenefit(orderDto, visitDateDto);
+
+        TotalBenefitAmountDto totalBenefitAmountDto = showTotalBenefitAmount(benefitsDto);
+        showPostDiscountAmount(preDiscountAmountDto, totalBenefitAmountDto);
+
+        showEventBadge(totalBenefitAmountDto);
+    }
+
+    private void showOrderedMenu(final OrderDto orderDto) {
         outputView.printOrderedMenu(orderDto);
+    }
+
+    private PreDiscountAmountDto showPreDiscountAmount(final OrderDto orderDto) {
         PreDiscountAmountDto preDiscountAmountDto = service.generatePreDiscountAmount(orderDto);
         outputView.printPreDiscountAmount(preDiscountAmountDto);
+        return preDiscountAmountDto;
+    }
+
+    private BenefitsDto showGiveawayAndBenefit(
+            final OrderDto orderDto,
+            final VisitDateDto visitDateDto
+    ) {
         BenefitsDto benefitsDto = service.generateBenefits(visitDateDto, orderDto);
         GiveawayMenuDto giveawayMenuDto = service.generateGiveAway(benefitsDto);
         outputView.printGiveaway(giveawayMenuDto);
         outputView.printBenefits(benefitsDto);
+        return benefitsDto;
+    }
+
+    private TotalBenefitAmountDto showTotalBenefitAmount(final BenefitsDto benefitsDto) {
         TotalBenefitAmountDto totalBenefitAmountDto = service.generateTotalBenefitAmount(benefitsDto);
         outputView.printTotalBenefitAmount(totalBenefitAmountDto);
+        return totalBenefitAmountDto;
+    }
+
+    private void showPostDiscountAmount(
+            final PreDiscountAmountDto preDiscountAmountDto,
+            final TotalBenefitAmountDto totalBenefitAmountDto
+    ) {
         PostDiscountAmountDto postDiscountAmountDto = service.generatePostDiscountAmount(
                 preDiscountAmountDto,
                 totalBenefitAmountDto
         );
         outputView.printPostDiscountAmount(postDiscountAmountDto);
+    }
+
+    private void showEventBadge(final TotalBenefitAmountDto totalBenefitAmountDto) {
         EventBadgeDto eventBadgeDto = service.generateBadge(totalBenefitAmountDto);
         outputView.printBadge(eventBadgeDto);
     }
